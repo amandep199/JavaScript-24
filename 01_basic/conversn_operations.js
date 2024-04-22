22294c5a8bc33fcaa1f2a0e840ec6c55728766fe
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-04-22 13:57:54 +0000

    conversion of datatypes

---
+++ 01_basic/conversn_operations.js
@@ -0,0 +1,23 @@
+// let score = "33ab"
+
+// console.log(typeof(score))
+
+
+// let valueInNumber= Number(score)          // get the value in number only
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//"33"=33
+//"33abc"= NaN
+// true=1 and false =0
+// "" = false
+// "wer"= true
+
+let Logedin = "23"
+
+let booleanvalue= Boolean(Logedin)
+
+console.log(typeof booleanvalue)
+console.log(booleanvalue)
