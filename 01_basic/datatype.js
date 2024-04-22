8b9bf3316994e68da20e6b523c24096716ae81e8
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-04-22 13:34:04 +0000

    data types

---
+++ 01_basic/datatype.js
@@ -0,0 +1,15 @@
+"use strict" ;  //treat all code as newer version
+//alert(3+3) //we are using nodejs not browser 
+
+let name ="string";
+let age =4
+
+let enter = true;
+
+//null = standalone value   means temp=null; not 0 degreee
+//undefined = no valu assign only declare
+//symbol=unique
+
+console.log(typeof("aman"));
+console.log(typeof(null));      //type=objecrt
+console.log(typeof(undefined));  //type= undefined
