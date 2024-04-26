1bb00e652c64378915a7219bc3a8b9c9b8a7732e
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-04-26 13:58:22 +0000

    summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,44 @@
+// primitive       call by value
+
+
+// 7 types - String , number , boolean ,undefined, null , symbol, bigInt
+
+//symbol ex
+
+
+// let id = Symbol(11223)
+// let newId= Symbol(234234)
+
+// console.log(id===newId)
+
+
+// //bigint ex
+
+// let bigInt = 7398453984n
+
+// console.log(typeof(bigInt))
+//refernce non-primitive
+
+
+//Arrays, object
+
+
+
+// const myClass=["Name", "Class", "Roll no"]
+
+//      let Student_name={
+
+//         name:"aman",
+//         class:"mca",
+
+     
+//      }
+
+const myFun= function(){
+
+   let  roll_no = 2
+
+   console.log(roll_no)
+}
+
+ 
