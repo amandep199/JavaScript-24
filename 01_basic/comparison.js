f41a94edfbde2988daad406ec2e08a6ad94a72a7
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-04-26 13:29:42 +0000

    comparison

---
+++ 01_basic/comparison.js
@@ -0,0 +1,16 @@
+// console.log("2"<1)
+// console.log("02">1)
+
+
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null>=0)
+
+
+
+//=== value strictly check
+
+
+console.log(1===2)
+console.log(2===2)
+console.log("2"===2)
