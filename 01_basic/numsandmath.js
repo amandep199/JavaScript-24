a48f9e65ad4c05f1be0a33fa06ac8d4f986e5842
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-01 10:16:58 +0000

    mathandnum

---
+++ 01_basic/numsandmath.js
@@ -0,0 +1,35 @@
+// const score = 300
+
+// const newNum = new Number(100)
+
+
+// console.log(newNum.toString().length)
+// console.log(newNum.toFixed(1))
+
+
+
+// const otherNum = 123.89
+
+// console.log(otherNum.toPrecision(4))
+
+
+// const hund=100000
+
+// console.log(hund.toLocaleString('en-IN'))
+
+
+
+
+
+//============MAths==================================
+
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log((Math.random()*10)  + 1)
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))  + min)
