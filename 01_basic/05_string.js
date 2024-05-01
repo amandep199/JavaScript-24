159474523b3141115365367560c1f90876b4efc6
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-01 10:03:13 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,22 @@
+// const name = "aman"
+
+// const myAge = 21
+
+// console.log(name[0])
+
+
+const myName = String("Amandeep")
+
+// console.log(myName)
+// console.log(myName.toUpperCase())
+// console.log(myName.__proto__)
+
+const newString = myName.substring(0,3)
+console.log(newString)
+
+const anotString=myName.slice(-7,4)
+console.log(anotString)
+
+// console.log( name + myAge)
+
+// console.log(`my name is ${name} and my age is ${myAge}`)
