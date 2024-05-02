d997159f2da18010646cdafc070fb7dc7a58b203
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-02 14:18:05 +0000

    date

---
+++ 01_basic/dateandtime.js
@@ -0,0 +1,18 @@
+// let mydate = new Date()
+
+// console.log(mydate.toString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toDateString())
+
+
+let createDate = new Date("2023-01-12")
+
+console.log(createDate.toLocaleDateString())
+
+
+
+
+let myTimeStamp = Date.now()
+
+console.log(createDate.getTime())
