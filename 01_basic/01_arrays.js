cf521b8d46a3b1e340498e4dbcf59aff9d808ef0
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-07 10:28:25 +0000

    arrays

---
+++ 01_basic/01_arrays.js
@@ -0,0 +1,30 @@
+const ind_team = ["Kohli","Chahal","Pant","Bumrah","Surya"]
+
+const aus_team = ["Warner","Maxwell","Starc","Marsh","Zampa"]
+
+
+// ind_team.push(aus_team)
+
+// const new_teaam = ind_team.concat(aus_team)
+
+
+
+// console.log(new_teaam)
+
+
+// const all_new = [...aus_team,...ind_team]
+
+
+// console.log(all_new)
+
+
+
+// const new_2 = [1,2,3,[3,4,2],4,5,7,[4,4,6,[6,778,89,8],78,67]]
+
+
+// console.log(new_2.flat(Infinity))
+
+
+console.log(Array.from("Amanndn"))
+
+console.log(Array.isArray("Amanndn"))
