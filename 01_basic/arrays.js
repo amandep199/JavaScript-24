3ee56003101e75e5a85aab914504f553da29393f
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-07 09:56:10 +0000

    arrays

---
+++ 01_basic/arrays.js
@@ -0,0 +1,51 @@
+const myArray=[3,4,5, "Aman", 'True']
+
+//  const myArr2 = new Array()
+
+// console.log(myArr2)
+// console.log(myArray[3])
+// console.log(myArray)
+
+
+//arrays methods
+
+// myArray.push(3)
+// myArray.pop()
+// myArr2.push(3)
+// console.log(myArr2)
+
+
+//myArray.unshift("mann")
+// myArray.shift()
+// console.log(myArray)
+
+
+// console.log(myArray.includes(3))
+
+
+// const newArri = myArray.join()
+
+// console.log(typeof newArri )
+
+
+// Slice and Splice
+
+
+console.log("A ", myArray)
+
+
+const my1 = myArray.slice(1,3)
+
+console.log(my1)
+console.log("B ", myArray)
+
+
+ const my2 = myArray.splice(1,3)
+
+ console.log("C ", myArray)
+ console.log(my2)
+
+
+
+
+
