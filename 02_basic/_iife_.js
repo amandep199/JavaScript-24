d21092743bd83d4f3d1b29d0805ac038f1b2e1d6
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-17 12:02:29 +0000

    iffe

---
+++ 02_basic/_iife_.js
@@ -0,0 +1,16 @@
+//Immediately Ivoked Functions Expressions      ///global scope ke pollution ko hatane k liye
+//named iffe
+(function chai(){
+
+    console.log(`DB connected`)
+
+}) ()
+
+
+//  (   chai1 = (name) =>{
+//     console.log(`DB connected2,${name }`)
+
+
+//  })("amannn")
+
+ 
