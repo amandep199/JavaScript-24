ddc029d7481e2979b12cc5df705bf3ded1abda05
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-18 11:53:33 +0000

    controlflow

---
+++ 03_controlflow/one.js
@@ -0,0 +1,21 @@
+const score = 200
+
+
+if(score>100){
+
+    const power = "fly"
+console.log(`user power:${power}`)
+
+}
+
+
+
+const isUserlogedin = true
+const isUserGpay =  true
+
+
+if(isUserGpay && isUserGpay){
+
+    console.log("perimt to log in")
+
+}
