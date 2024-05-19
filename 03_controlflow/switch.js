99271e2d635ef99ec4466adcf7a2097adcf1d912
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-19 11:13:20 +0000

    switch

---
+++ 03_controlflow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+         break;
+
+    default:
+        console.log("thulla")
+        break;
+}
