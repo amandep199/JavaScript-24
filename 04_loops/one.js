d7a0490d9914ed968bd502531b71c15aac4990b5
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-20 12:55:48 +0000

    forloop

---
+++ 04_loops/one.js
@@ -0,0 +1,24 @@
+for (let index = 0; index <= 10; index++) {
+    const element = 2*index;
+// console.log(element)
+}
+
+
+
+let myHero = [ "Shaktiman","Hanuman","Nagraj","Spiderman","Naruto"]
+
+
+for (let  index = 0; index < myHero.length; index++) {
+    const element = myHero[3];
+   
+    
+    // console.log(element)
+}
+
+
+for (let index = 1; index < 20; index++) {
+    console.log(`value is : ${index} `)
+    
+
+    if(index==5){ console.log("printed") }
+}
