321d08395f6fab34d4a42032e4c077a25ed36ff0
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-20 11:22:05 +0000

    truthy

---
+++ 03_controlflow/truthy.js
@@ -0,0 +1,49 @@
+const userEmail = []
+//  userEmail =  true
+
+
+// if (userEmail) {
+    
+//     console.log("logedin")
+// }else {
+
+//     console.log("not login")
+// }
+
+// if(userEmail.length===0){
+
+//     console.log("array is empty")
+// }
+
+// const myObj = {}
+
+// if(Object.keys(myObj).length===0){
+
+//     console.log("empty object")
+
+// }
+
+//falsy values
+// 0 , -0 , false, Bigint 0n , Nan, Null, undefined, ""
+
+
+
+// truthy values
+
+// "0", 'false', " ", [], {}
+
+
+//Nullish Coalescing Operator(??):null, undefined
+
+
+
+let val1 ;
+
+// var1 = 10 ?? 4
+// var1 = null ?? 4
+val1 = undefined ?? 7
+
+
+console.log(val1)
+
+
