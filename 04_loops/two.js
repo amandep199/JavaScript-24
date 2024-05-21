8d46d749e829209edb24b99f3f5ed43fd75a7b1a
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-21 11:43:12 +0000

    do while

---
+++ 04_loops/two.js
@@ -0,0 +1,27 @@
+
+let index = 0;
+while (index<10) {
+    // console.log(`print this number ${index}`)
+    index = index +3
+} 
+
+let myArr= ["aman","raman","dhoni","kohli"]
+
+let marr=0
+while (marr<myArr.length) {
+
+    // console.log(myArr[marr])
+    
+    marr++
+}
+
+
+let level =11
+
+ do {
+    console.log(`level is ${level }`)
+level ++
+
+} while (level <10);
+
+console.log("reach at level 10")
