1d4af141d4dc939325500ab2ac51c126fe522ebb
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-22 10:43:40 +0000

    for each

---
+++ 04_loops/three.js
@@ -0,0 +1,24 @@
+
+const arr =[1,2,3,4,5]
+
+for (const value of arr) {
+    
+    // console.log(`value is ${value}`)
+}
+
+
+const greetings = "amann"
+for (const greet of greetings) {
+
+    // console.log(`Each char is ${greet}`)
+    
+}
+
+
+
+//+++++++++++++++++++++++Maps_+++++++++++++
+
+
+const map = new Map()
+
+
