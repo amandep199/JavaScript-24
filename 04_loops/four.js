7e8681558ffb5360edf171cb596454ca42547b26
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-22 11:11:14 +0000

    for in

---
+++ 04_loops/four.js
@@ -0,0 +1,21 @@
+const myLang ={
+    js:"for java",
+    py:"for python",
+    cpp:"for c++"
+}
+
+
+for (const key in myLang) {
+//  console.log(`lang are ${key} ${myLang[key]} `)
+}
+
+
+
+//+++++++++++++++++++for in loop For Arrays
+
+
+const villages =["ludas","dobhi","kurdi"]
+
+for (const key in villages) {
+   console.log(` ${villages[key]}`)
+}
