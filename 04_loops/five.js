f8cfad11760f866f0ee87888583316199d6068fa
amandep199 <167500173+amandep199@users.noreply.github.com>
2024-05-22 11:54:14 +0000

    arrays for each

---
+++ 04_loops/five.js
@@ -0,0 +1,47 @@
+const villages =["ludas","kurdi","shahpur","donhi","kirtan"]
+
+
+//function bnaana s binaa naam ka arr uske ander arrays ko koi bhi naam se pass krna ss bsss
+//  villages.forEach( function (gaam_k_naam){
+
+//      console.log(gaam_k_naam)
+//  })
+
+// function printme(item){
+
+//     console.log(item)
+
+// }
+
+
+
+// villages.forEach(printme)
+
+
+villages.forEach( (items,index,arr) => {
+    // console.log(items,index,arr)
+});
+
+
+const myCoding =[
+
+    {
+        lnaguagename:"Javascript",
+        languageFilename:"js"
+    },
+
+    {
+        lnaguagename:"Java",
+        languageFilename:"java"
+    },
+    {
+        lnaguagename:"python",
+        languageFilename:"py"
+    }
+]
+
+
+myCoding.forEach( (items)=>{
+ 
+    console.log(items)
+})
